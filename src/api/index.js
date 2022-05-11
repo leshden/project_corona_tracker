0f77c21f885dbd4b754fdbcd2d7364c8f355d3b5
Viktor Denikin <leshden@yandex.ru>
2022-05-11 14:58:53 +0300

    replace axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
@@ -10,8 +9,8 @@ export const fetchData = async (country) => {
   }
 
   try {
-    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
-
+    const data = await fetch(changeableUrl);
+    const {confirmed, recovered, deaths, lastUpdate} = await data.json();
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
     return error;
@@ -21,8 +20,8 @@ export const fetchData = async (country) => {
 // Instead of Global, it fetches the daily data for the US
 export const fetchDailyData = async () => {
     try {
-      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
-
+      const rawData = await fetch('https://api.covidtracking.com/v1/us/daily.json');
+      const  data  = await rawData.json();
       return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
     } catch (error) {
       return error;
@@ -31,8 +30,8 @@ export const fetchDailyData = async () => {
 
 export const fetchCountries = async () => {
   try {
-    const { data: { countries } } = await axios.get(`${url}/countries`);
-
+    const data = await fetch(`${url}/countries`);
+    const {countries} = await data.json();
     return countries.map((country) => country.name);
   } catch (error) {
     return error;
